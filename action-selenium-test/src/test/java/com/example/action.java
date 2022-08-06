c8be00ae598bccc598ab397c7f4717d8d729ae88
Lmori07 <33818263+Lmori07@users.noreply.github.com>
2022-08-06 07:53:50 -0400

    Upate

--- action-selenium-test/src/test/java/com/example/action.java
+++ action-selenium-test/src/test/java/com/example/action.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
